be9fb5dd91c2865f14d35ffc3f281b64309b190e
Ezequiel Barreto <ezebar37@gmail.com>
2020-05-02 15:36:01 -0300

    CotizaTerre

---
+++ VariablesOperaciones/CotizaciónTerrenoCs/CotizaciónTerrenoCs/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CotizaciónTerrenoCs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ulong super, ancho, lado, longitud, obs, precio, final;
+
+            Console.Write("Ingrese el valor del metro cuadrado: $");
+            precio = Convert.ToUInt16(Console.ReadLine());
+
+            Console.Write("Ingrese la dimensión del terreno (1=Cuadrado/2=rectángulo): ");
+            obs = Convert.ToUInt32(Console.ReadLine());
+            if (obs == 1)
+            {
+                Console.Write("Ingrese el LADO del terreno: ");
+                lado = Convert.ToUInt32(Console.ReadLine());
+                super = (lado) * (lado);
+            }
+            else
+            {
+                Console.Write("Ingrese el ANCHO del terreno: ");
+                ancho = Convert.ToUInt32(Console.ReadLine());
+                Console.Write("Ingrese la LONGITUD del terreno: ");
+                longitud = Convert.ToUInt32(Console.ReadLine());
+                super = ancho * longitud;
+
+            }
+
+            final = precio * super;
+
+            Console.WriteLine("La superficie de su terreno es de " + super + " metros cuadrados");
+            Console.WriteLine("El precio a pagar este terreno es de $" + final);
+
+            Console.ReadKey();
+        }
+    }
+}
