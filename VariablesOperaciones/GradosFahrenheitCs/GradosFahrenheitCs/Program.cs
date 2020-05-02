85a051520598ecaa9f06cba08c2d615963100f67
Ezequiel Barreto <ezebar37@gmail.com>
2020-05-02 15:43:27 -0300

    GradosCs

---
+++ VariablesOperaciones/GradosFahrenheitCs/GradosFahrenheitCs/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GradosFahrenheitCs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ulong grados;
+            double Kelvin = 273.15;
+            double R;
+
+            Console.Write("Ingrese los grados en °C: ");
+            grados = Convert.ToUInt16(Console.ReadLine());
+
+            R = grados + Kelvin;
+
+            Console.Write(grados);
+            Console.Write("°C equivalen a ");
+            Console.Write(R);
+            Console.WriteLine("K");
+        }
+    }
+}
